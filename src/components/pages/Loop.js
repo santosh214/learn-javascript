d8c5d78339e88869ae7af7c80689a0ee47ef93c8
santosh214 <santoshsingh74827@gmail.com>
2022-11-30 23:19:35 +0530

    add question

--- src/components/pages/Loop.js
+++ src/components/pages/Loop.js
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Checkbox, Grid, Typography } from "@mui/material";
 import React from "react";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
@@ -13,9 +13,11 @@ export default function Loop() {
   }));
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={8}>
-          <Item>xs=8</Item>
+      <Grid container spacing={2} >
+        <Grid item xs={12}>
+            <Typography align="center" variant="h5" color="initial">Loops in JS ?</Typography>
+             While       <Checkbox  />
+
         </Grid>
         <Grid item xs={4}>
           <Item>xs=4</Item>
