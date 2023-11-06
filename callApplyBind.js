7fffab3936a3f68462c3b342bc80d3de495af915
santosh214 <santoshsingh74827@gmail.com>
2023-11-07 00:20:49 +0530

    shallow vs deep

--- callApplyBind.js
+++ callApplyBind.js
@@ -1,3 +1,7 @@
+// The difference between Call, Apply and Bind can be explained with below examples,
+
+
+// Call: The call() method invokes a function with a given this value and arguments provided one by one
 let employee1 = {
     name: 'santosh',
     age: 28
@@ -8,9 +12,12 @@ let employee2 = {
 }
 
 function invite(grt1, grt2) {
-    console.log(grt1 + " " + this.name + " " + grt2 + " " + this.name)
-    console.log("this:", this)
+    console.log("grt2:", grt2)
+    console.log("grt1:", grt1)
+    // console.log(grt1 + " " + this.name + " " + grt2 + " " + this.name)
+    // console.log("this:", this)
 }
 
 
-invite.call(employee1, "Hello","How are you")
+invite.call("Hello", "How are you")
+// invite.call(employee2, "hello", "what are you doing")
